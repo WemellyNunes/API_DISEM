fd992c5e2668c744cf4852c7bd980d908f1e7a2b
Wemelly Nunes <wemellyohanna@gmail.com>
2024-07-15 11:24:18 -0300

    init

---
+++ src/test/java/com/disem/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disem.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
