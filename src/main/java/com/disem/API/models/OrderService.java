b18e86de419ef0c0a06255e00a7ac90439235457
Wemelly Nunes <wemellyohanna@gmail.com>
2024-07-15 21:52:47 -0300

    commit inicial

---
+++ src/main/java/com/disem/API/models/OrderService.java
@@ -0,0 +1,17 @@
+package com.disem.API.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "TB_ORDER_SERVICE")
+public class OrderService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+}
