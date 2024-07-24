066dc12105523e0b07bc15d587eb3ad0e23eed1f
Wemelly Nunes <wemellyohanna@gmail.com>
2024-07-24 11:13:31 -0300

    feat: add o repository e service do ChecklistCorrective

---
+++ src/main/java/com/disem/API/repositories/ChecklistCorrectiveRepository.java
@@ -0,0 +1,12 @@
+package com.disem.API.repositories;
+
+import com.disem.API.models.ChecklistCorrectiveModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ChecklistCorrectiveRepository extends JpaRepository<ChecklistCorrectiveModel, UUID> {
+
+}
---
+++ src/main/java/com/disem/API/services/ChecklistCorrectiveService.java
@@ -0,0 +1,37 @@
+package com.disem.API.services;
+
+import com.disem.API.models.ChecklistCorrectiveModel;
+import com.disem.API.repositories.ChecklistCorrectiveRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ChecklistCorrectiveService {
+
+    @Autowired
+    ChecklistCorrectiveRepository checklistCorrectiveRepository;
+
+    @Transactional
+    public ChecklistCorrectiveModel save(ChecklistCorrectiveModel checklistCorrectiveModel) {
+        return checklistCorrectiveRepository.save(checklistCorrectiveModel);
+    }
+
+    public Page<ChecklistCorrectiveModel> findAll(Pageable pageable) {
+        return checklistCorrectiveRepository.findAll(pageable);
+    }
+
+    public Optional<ChecklistCorrectiveModel> findById(UUID id) {
+        return checklistCorrectiveRepository.findById(id);
+    }
+
+    public void delete(ChecklistCorrectiveModel checklistCorrectiveModel) {
+        checklistCorrectiveRepository.delete(checklistCorrectiveModel);
+    }
+
+}
