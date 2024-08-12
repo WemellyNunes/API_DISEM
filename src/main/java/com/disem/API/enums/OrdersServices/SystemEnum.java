0174b19da33a9c96400183ce8b0acdc9cfa6a709
Wemelly Nunes <wemellyohanna@gmail.com>
2024-08-12 17:24:40 -0300

    refacto: alterando os tipos de sistemas

--- src/main/java/com/disem/API/enums/OrdersServices/SystemEnum.java
+++ src/main/java/com/disem/API/enums/OrdersServices/SystemEnum.java
@@ -3,7 +3,7 @@ package com.disem.API.enums.OrdersServices;
 public enum SystemEnum {
     CIVIL,
     ELETRICO,
-    HIDRAULICO,
-    PLUVIAL,
-    SANITARIO
+    HIDROSANITARIO,
+    REFRIGERACAO,
+    MISTO
 }
