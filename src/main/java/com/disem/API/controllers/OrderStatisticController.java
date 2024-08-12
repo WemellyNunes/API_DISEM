4184a2fcea46998116d4bb2764fcc64dcc65dfe1
Wemelly Nunes <wemellyohanna@gmail.com>
2024-08-12 17:24:13 -0300

    feat: add mais serviços para o dashboard

--- src/main/java/com/disem/API/controllers/OrderStatisticController.java
+++ src/main/java/com/disem/API/controllers/OrderStatisticController.java
@@ -3,6 +3,7 @@ package com.disem.API.controllers;
 
 import com.disem.API.enums.OrdersServices.*;
 import com.disem.API.services.OrderServiceService;
+import com.disem.API.services.OrderStatisticService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,53 +21,53 @@ import java.util.Map;
 public class OrderStatisticController {
 
     @Autowired
-    OrderServiceService orderServiceService;
+    OrderStatisticService orderStatisticService;
 
     @GetMapping("/statistics/classification")
     public ResponseEntity<Map<ClassEnum, Integer>> getClassStatistics() {
-        Map<ClassEnum, Integer> ordersClassQuantities = orderServiceService.findOrdersByClassForCurrentMonth();
+        Map<ClassEnum, Integer> ordersClassQuantities = orderStatisticService.findOrdersByClassForCurrentMonth();
         return new ResponseEntity<>(ordersClassQuantities, HttpStatus.OK);
     }
 
     @GetMapping("/statistics/typeMaintenance")
     public ResponseEntity<Map<TypeMaintenanceEnum, Double>> getTypeStatistics() {
-        Map<TypeMaintenanceEnum, Double> ordersTypeQuantities = orderServiceService.findOrdersByTypeForCurrentMonth();
+        Map<TypeMaintenanceEnum, Double> ordersTypeQuantities = orderStatisticService.findOrdersByTypeForCurrentMonth();
         return new ResponseEntity<>(ordersTypeQuantities, HttpStatus.OK);
     }
 
     @GetMapping("/statistics/system")
     public ResponseEntity<Map<SystemEnum, Double>> getSystemStatistics() {
-        Map<SystemEnum, Double> ordersSystemQuantities = orderServiceService.findOrdersBySystemForCurrentMonth();
+        Map<SystemEnum, Double> ordersSystemQuantities = orderStatisticService.findOrdersBySystemForCurrentMonth();
         return new ResponseEntity<>(ordersSystemQuantities, HttpStatus.OK);
     }
 
     @GetMapping("/statistics/sipacOrders")
     public ResponseEntity<Map<String, Long>> getSipacOrdersCount(){
-        Map<String, Long> ordersSipacQuantities = orderServiceService.findOrdersSipacForStatus(OriginEnum.SIPAC, StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
+        Map<String, Long> ordersSipacQuantities = orderStatisticService.findOrdersSipacForStatus(OriginEnum.SIPAC, StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
         return new ResponseEntity<>(ordersSipacQuantities, HttpStatus.OK);
     }
 
     @GetMapping("/statistics/ordersMonth")
     public ResponseEntity<Map<String, Long>> getMonthOrdersCount(){
-        Map<String, Long> monthOrders = orderServiceService.findOrdersByStatusForCurrentMonth(StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
+        Map<String, Long> monthOrders = orderStatisticService.findOrdersByStatusForCurrentMonth(StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
         return new ResponseEntity<>(monthOrders, HttpStatus.OK);
     }
 
     @GetMapping("/statistics/weekOrders")
     public ResponseEntity<Map<String, Long>> getWeekOrdersCount() {
-        Map<String, Long> weekOrders = orderServiceService.findOrdersByStatusesForCurrentWeek(StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
+        Map<String, Long> weekOrders = orderStatisticService.findOrdersByStatusesForCurrentWeek(StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
         return new ResponseEntity<>(weekOrders, HttpStatus.OK);
     }
 
     @GetMapping("/statistics/todayOrders")
     public ResponseEntity<Map<String, Long>> getTodayOrdersCount(){
-        Map<String, Long> todayOrders = orderServiceService.findOrdersByStatusesForToday(StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
+        Map<String, Long> todayOrders = orderStatisticService.findOrdersByStatusesForToday(StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
         return new ResponseEntity<>(todayOrders, HttpStatus.OK);
     }
 
     @GetMapping("/statistics/yearOrders")
     public ResponseEntity<Map<String,Long>> getYearOrdersCount() {
-        Map<String, Long> yearOrders = orderServiceService.findOrdersByStatusesForCurrentYear(StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
+        Map<String, Long> yearOrders = orderStatisticService.findOrdersByStatusesForCurrentYear(StatusEnum.A_ATENDER, StatusEnum.FINALIZADO);
         return new ResponseEntity<>(yearOrders, HttpStatus.OK);
     }
 
---
+++ src/main/java/com/disem/API/services/OrderStatisticService.java
@@ -0,0 +1,115 @@
+package com.disem.API.services;
+
+import com.disem.API.enums.OrdersServices.*;
+import com.disem.API.repositories.OrderServiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class OrderStatisticService {
+
+    @Autowired
+    OrderServiceRepository orderServiceRepository;
+
+    public Map<ClassEnum, Integer> findOrdersByClassForCurrentMonth() {
+        LocalDate startDate = LocalDate.now().withDayOfMonth(1);
+        LocalDate endDate = startDate.plusMonths(1).minusDays(1);
+
+        Map<ClassEnum, Integer> ordersClassQuantities = new HashMap<>();
+        for (ClassEnum classEnum : ClassEnum.values()) {
+            ordersClassQuantities.put(classEnum, orderServiceRepository.countByClassificationAndDateBetween(classEnum, startDate, endDate));
+        }
+        return ordersClassQuantities;
+    }
+
+
+    public Map<TypeMaintenanceEnum, Double> findOrdersByTypeForCurrentMonth() {
+        LocalDate startDate = LocalDate.now().withDayOfMonth(1);
+        LocalDate endDate = startDate.plusMonths(1).minusDays(1);
+
+        long totalOrders = orderServiceRepository.countByDateBetween(startDate, endDate);
+
+        Map<TypeMaintenanceEnum, Double> ordersTypeQuantities = new HashMap<>();
+        for (TypeMaintenanceEnum typeMaintenanceEnum : TypeMaintenanceEnum.values()) {
+            long count = orderServiceRepository.countByTypeMaintenanceAndDateBetween(typeMaintenanceEnum, startDate, endDate);
+            double percentage = Math.round((double) count / totalOrders * 100);
+            ordersTypeQuantities.put(typeMaintenanceEnum, percentage);
+        }
+        return ordersTypeQuantities;
+    }
+
+
+    public Map<SystemEnum, Double> findOrdersBySystemForCurrentMonth() {
+        LocalDate startDate = LocalDate.now().withDayOfMonth(1);
+        LocalDate endDate = startDate.plusMonths(1).minusDays(1);
+
+        long totalOrders = orderServiceRepository.countByDateBetween(startDate, endDate);
+
+        Map<SystemEnum, Double> ordersSystemQuantities = new HashMap<>();
+        for (SystemEnum systemEnum : SystemEnum.values()) {
+            long count = orderServiceRepository.countBySystemAndDateBetween(systemEnum, startDate, endDate);
+            double percentage = Math.round((double) count / totalOrders * 100);
+            ordersSystemQuantities.put(systemEnum, percentage);
+        }
+        return ordersSystemQuantities;
+    }
+
+
+    public Map<String, Long> findOrdersSipacForStatus(OriginEnum origin, StatusEnum status1, StatusEnum status2){
+        long toAttendCount = orderServiceRepository.countByOriginAndStatus(origin, status1);
+        long finalizedCount = orderServiceRepository.countByOriginAndStatus(origin, status2);
+
+        Map<String, Long> ordersSipac = new HashMap<>();
+        ordersSipac.put("APROVADAS", toAttendCount);
+        ordersSipac.put("FINALIZADAS", finalizedCount);
+        return ordersSipac;
+    }
+
+
+    public Map<String, Long> findOrdersByStatusesForPeriod(LocalDate startDate, LocalDate endDate, StatusEnum status1, StatusEnum status2){
+        long toAttendCount = orderServiceRepository.countByStatusAndDateBetween(status1, startDate, endDate);
+        long finalizedCount = orderServiceRepository.countByStatusAndDateBetween(status2, startDate, endDate);
+
+        Map<String, Long> ordersPeriod = new HashMap<>();
+        ordersPeriod.put("A_ATENDER", toAttendCount);
+        ordersPeriod.put("FINALIZADAS", finalizedCount);
+        return ordersPeriod;
+    }
+
+
+    public Map<String, Long> findOrdersByStatusForCurrentMonth(StatusEnum status1, StatusEnum status2) {
+        LocalDate startDate = LocalDate.now().withDayOfMonth(1);
+        LocalDate endDate = startDate.plusMonths(1).minusDays(1);
+
+        return findOrdersByStatusesForPeriod(startDate, endDate, status1, status2);
+    }
+
+
+    public Map<String, Long> findOrdersByStatusesForCurrentWeek(StatusEnum status1, StatusEnum status2) {
+        LocalDate startDate = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+        LocalDate endDate = startDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY));
+
+        return findOrdersByStatusesForPeriod(startDate, endDate, status1, status2);
+    }
+
+
+    public Map<String, Long> findOrdersByStatusesForToday(StatusEnum status1, StatusEnum status2) {
+        LocalDate today = LocalDate.now();
+        return findOrdersByStatusesForPeriod(today, today, status1, status2);
+    }
+
+
+    public Map<String, Long> findOrdersByStatusesForCurrentYear(StatusEnum status1, StatusEnum status2) {
+        LocalDate startDate = LocalDate.now().with(TemporalAdjusters.firstDayOfYear());
+        LocalDate endDate = startDate.with(TemporalAdjusters.lastDayOfYear());
+
+        return findOrdersByStatusesForPeriod(startDate, endDate, status1, status2);
+    }
+
+}
