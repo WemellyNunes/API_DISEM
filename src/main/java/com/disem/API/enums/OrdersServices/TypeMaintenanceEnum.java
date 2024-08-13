77ec955c5368ead5cf549169ccbd5d208636e04b
Wemelly Nunes <wemellyohanna@gmail.com>
2024-08-13 16:42:22 -0300

    refactor: alterando os nomes dos enum

--- src/main/java/com/disem/API/enums/OrdersServices/TypeMaintenanceEnum.java
+++ src/main/java/com/disem/API/enums/OrdersServices/TypeMaintenanceEnum.java
@@ -1,17 +1,7 @@
 package com.disem.API.enums.OrdersServices;
 
 public enum TypeMaintenanceEnum {
-    MANUTENCAO_CORRETIVA("Manutenção corretiva"),
-    MANUTENCAO_PREVENTIVA("Manutenção preventiva"),
-    EXTRAMANUTENCAO("Extramanutenção");
-
-    private String legend;
-
-    TypeMaintenanceEnum(String legend){
-        this.legend = legend;
-    }
-
-    public String getLegend(){
-        return this.legend;
-    }
+    CORRETIVA,
+    PREVENTIVA,
+    EXTRAMANUTENCAO
 }
