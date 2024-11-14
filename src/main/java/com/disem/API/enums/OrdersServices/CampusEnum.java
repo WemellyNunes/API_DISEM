8fa33858fa9e61a1c8341270a86278194370904f
wemelly <wemellyohanna@gmail.com>
2024-11-14 11:39:55 -0300

    refactor: alterando enums do atributo Campus

--- src/main/java/com/disem/API/enums/OrdersServices/CampusEnum.java
+++ src/main/java/com/disem/API/enums/OrdersServices/CampusEnum.java
@@ -1,19 +1,33 @@
 package com.disem.API.enums.OrdersServices;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum CampusEnum {
 
-    MARABA("Marabá"),
-    SANTANA_DO_ARAGUAIA("Santana do Araguaia"),
-    XINGUARA("Xinguara"),
-    SAO_FELIX_DO_XINGU("São Félix do Xingu"),
-    RONDON_DO_PARA("Rondon");
+    MARABA("MARABA"),
+    SANTANA_DO_ARAGUAIA("SANTANA DO ARAGUAIA"),
+    XINGUARA("XINGUARA"),
+    SAO_FELIX_DO_XINGU("SAO FELIX DO XINGU"),
+    RONDON_DO_PARA("RONDON"),;
 
     private final String name;
 
     CampusEnum(String name) {
         this.name = name;
     }
+    @JsonValue
     public String getName() {
         return name;
+    }
+
+    @JsonCreator
+    public static CampusEnum fromName(String name) {
+        for (CampusEnum campusEnum : CampusEnum.values()) {
+            if (campusEnum.getName().equalsIgnoreCase(name)) {
+                return campusEnum;
+            }
+        }
+        throw new IllegalArgumentException("Campus desconhecido" + name);
     }
 }
